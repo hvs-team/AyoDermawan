a0ae144d3521a04a23fd2e3f1fea01ed8f73a58c
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2017-08-28 00:38:31 +0700

    28 August 2017 : 00:38

--- src/pages/donatur-sumbang/donatur-sumbang.ts
+++ src/pages/donatur-sumbang/donatur-sumbang.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
-
-import { DonaturHomePage } from '../donatur-home/donatur-home';
-import { DonaturListPage } from '../donatur-list/donatur-list';
-import { DonaturProfilPage } from '../donatur-profil/donatur-profil';
-
+import { IonicPage, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -12,11 +9,18 @@ import { DonaturProfilPage } from '../donatur-profil/donatur-profil';
 })
 export class DonaturSumbangPage {
 
-  tab1Root = DonaturHomePage;
-  tab2Root = DonaturListPage;
+  sumbang: string = "uang";
 
-  constructor() {
-
+  constructor(
+    // private vibration: Vibration,
+    public navCtrl: NavController, 
+    // public http: Http, 
+    public alertCtrl: AlertController, 
+    public navParams: NavParams, 
+    // public data: Data,
+    public loadCtrl: LoadingController) {
   }
+
+
 
 }
